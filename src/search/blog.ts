06377a007c4c5ba86734fc1fc82ce1535568df11
Michael Di Prisco <cadienvan@gmail.com>
2024-04-01 21:09:27 +0200

    feat: aggiunti blog a ricerca

--- src/search/blog.ts
+++ src/search/blog.ts
@@ -1,5 +1,7 @@
 import { create, insert } from "@orama/orama";
 import * as inevitabot from "../pages/blog/inevitabot.md";
+import * as asynchronousBatching from "../pages/blog/asynchronous-batching.md";
+import * as svalutazioneFrontend from "../pages/blog/svalutazione-frontend.md";
 
 export default async function fill() {
   const blogDB = await create({
@@ -17,6 +19,20 @@ export default async function fill() {
     date: inevitabot.frontmatter.date,
     content: inevitabot.rawContent(),
     url: "/blog/inevitabot",
+  });
+
+  await insert(blogDB, {
+    title: asynchronousBatching.frontmatter.title,
+    date: asynchronousBatching.frontmatter.date,
+    content: asynchronousBatching.rawContent(),
+    url: "/blog/asynchronous-batching",
+  });
+
+  await insert(blogDB, {
+    title: svalutazioneFrontend.frontmatter.title,
+    date: svalutazioneFrontend.frontmatter.date,
+    content: svalutazioneFrontend.rawContent(),
+    url: "/blog/svalutazione-frontend",
   });
 
   return { instance: blogDB, params: {
